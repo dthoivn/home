cd948faef126fbcb63227e7144b3cab7a3b89150
dthoivn <dthoivn@gmail.com>
2026-02-07 08:44:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -155,7 +155,6 @@ if(n.text.toLowerCase().includes(s)){
 list.innerHTML+=`
 <li>
 
-<!-- FILE LINK HERE -->
 <b>${i+1}. ${
 n.fileUrl
 ? `<a href="${n.fileUrl}" target="_blank">${n.text}</a>`
@@ -167,15 +166,6 @@ n.fileUrl
 <button onclick="edit('${n.id}','${n.text.replace(/'/g,"")}')">EDIT</button>
 <button onclick="del('${n.id}')">DEL</button>
 
-<b>${i+1}. ${
-n.fileUrl
-? `<a href="${n.fileUrl}" target="_blank">${n.text}</a>`
-: n.text
-}</b><br>
-
-<small>${n.time}</small><br>
-<button onclick="edit('${n.id}','${n.text.replace(/'/g,"")}')">EDIT</button>
-<button onclick="del('${n.id}')">DEL</button>
 </li>`;
 }
 });
@@ -295,6 +285,7 @@ load();
 reader.readAsBinaryString(file);
 
 });
+
 
 
 
